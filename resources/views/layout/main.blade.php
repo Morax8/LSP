b331b5c1b073a7499e2f2eae255b272b4ec26e8e
Morax8 <adityazianurrahman@gmail.com>
2024-02-13 08:19:10 +0700

    adding respnsive tag

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -2,6 +2,9 @@
 <html>
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>
         @yield('title')
     </title>
@@ -15,6 +18,7 @@
         rel="stylesheet">
 
 </head>
+
 
 <body>
     @include('partial.navbar')
--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -1,12 +1,13 @@
-<nav class="navbar navbar-expand-lg" style="margin-left: 20px; margin-right: 20px;">
-    <div class="container-fluid">
+<nav class="navbar navbar-expand-lg">
+    <div class="container-fluid style=" margin-left: 20px; margin-right: 20px;"">
         <a class="navbar-brand" href="/">Pelayanan Pengaduan Masyarakat</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
             aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav ms-lg-auto mb-2 mb-lg-0">
+                <!-- Changed ms-auto to ms-lg-auto -->
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="/">Home</a>
                 </li>
